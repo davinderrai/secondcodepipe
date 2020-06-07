2ad73ea024f4c9b28719e3b59b8a4032f1dfb4ae
Davinder Singh <simurgrai@gmail.com>
2020-06-07 07:23:44 +0530

    code

---
+++ build/precache-manifest.f6b062a6a30f83c6bfb81c9add0cb667.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "2c20cef7ff5c7fa99275",
+    "url": "/static/js/main.2c20cef7.chunk.js"
+  },
+  {
+    "revision": "063ce5bc9f8a30d66648",
+    "url": "/static/js/2.063ce5bc.chunk.js"
+  },
+  {
+    "revision": "2c20cef7ff5c7fa99275",
+    "url": "/static/css/main.b241fb25.chunk.css"
+  },
+  {
+    "revision": "41fdb7d5a5626833c189fc202a369cb3",
+    "url": "/index.html"
+  }
+];
